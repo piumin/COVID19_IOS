dd666c1f1b82a75d70c8e659e184b38083f5d909
piumin <67179701+piumin@users.noreply.github.com>
2020-09-12 07:36:23 -0700

    add and customise tab bar, navigation controllers

---
+++ COVID19.3/COVID19.3/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  COVID19.3
+//
+//  Created by Wickramasinghe PN on 9/12/20.
+//  Copyright © 2020 Wickramasinghe PN. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ COVID19.3/COVID19.3/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  COVID19.3
+//
+//  Created by Wickramasinghe PN on 9/12/20.
+//  Copyright © 2020 Wickramasinghe PN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+         
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
