04fd1a0373c2c29f3fa061753f3e3dc823cfb179
piumin <67179701+piumin@users.noreply.github.com>
2020-09-13 22:54:50 +0530

    fixing more constraints, navigation

--- COVID19.3/COVID19.3/ViewController.swift
+++ COVID19.3/COVID19.3/ViewController.swift
@@ -11,6 +11,9 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var vwContainerhome1:UIView!
+    @IBOutlet weak var vwContainerhome2:UIView!
+    @IBOutlet weak var vwContainerhome3:UIView!
+    @IBOutlet weak var vwContainerhome4:UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
         vwContainerhome1.layer.cornerRadius = 10.0
@@ -20,6 +23,30 @@ class ViewController: UIViewController {
         vwContainerhome1.layer.shadowRadius = 10.0
         vwContainerhome1.layer.shadowPath = UIBezierPath(rect: vwContainerhome1.bounds).cgPath
         vwContainerhome1.layer.shouldRasterize = true
+        
+        vwContainerhome2.layer.cornerRadius = 10.0
+        vwContainerhome2.layer.shadowColor = UIColor.gray.cgColor
+        vwContainerhome2.layer.shadowOffset = .zero
+        vwContainerhome2.layer.shadowOpacity = 0.8
+        vwContainerhome2.layer.shadowRadius = 10.0
+        vwContainerhome2.layer.shadowPath = UIBezierPath(rect: vwContainerhome2.bounds).cgPath
+        vwContainerhome2.layer.shouldRasterize = true
+        
+        vwContainerhome3.layer.cornerRadius = 10.0
+        vwContainerhome3.layer.shadowColor = UIColor.gray.cgColor
+        vwContainerhome3.layer.shadowOffset = .zero
+        vwContainerhome3.layer.shadowOpacity = 0.8
+        vwContainerhome3.layer.shadowRadius = 10.0
+        vwContainerhome3.layer.shadowPath = UIBezierPath(rect: vwContainerhome3.bounds).cgPath
+        vwContainerhome3.layer.shouldRasterize = true
+        
+        vwContainerhome4.layer.cornerRadius = 10.0
+        vwContainerhome4.layer.shadowColor = UIColor.gray.cgColor
+        vwContainerhome4.layer.shadowOffset = .zero
+        vwContainerhome4.layer.shadowOpacity = 0.8
+        vwContainerhome4.layer.shadowRadius = 10.0
+        vwContainerhome4.layer.shadowPath = UIBezierPath(rect: vwContainerhome4.bounds).cgPath
+        vwContainerhome4.layer.shouldRasterize = true
             }
     
 
